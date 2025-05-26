28c32de6fb7d0d40b6210e8632a7a369180b0b71
dmytr <dmytro.hrynchyshyn.oi.2023@lpnu.ua>
2025-05-26 16:15:26 +0300

    fix: socket link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io('https://alphaking.onrender.com');
 
 function App() {
   const [username, setUsername] = useState('');
