e637929f36a79299bf5a6f13459e23c64a4b9c9a
dmytr <dmytro.hrynchyshyn.oi.2023@lpnu.ua>
2025-05-29 00:14:29 +0300

    try2

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,12 +1,12 @@
 import { io } from "socket.io-client";
 
-// export const socket = io("https://alphaking.onrender.com", {
-//   transports: ["websocket"]
-// });
-
-const socket = io("http://localhost:5000", {
+export const socket = io("https://alphaking.onrender.com", {
   transports: ["websocket"]
 });
+
+// const socket = io("http://localhost:5000", {
+//   transports: ["websocket"]
+// });
 
 socket.on('connect', () => {
   console.log('Socket connected with ID:', socket.id);
